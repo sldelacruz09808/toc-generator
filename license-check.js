085b543d6cf43948c906f8c34d767464f5656d79
Bri808 <sldelacruz09@gmail.com>
2026-09-24 08:51:48 -1000

    Add files via upload

--- license-check.js
+++ license-check.js
@@ -16,8 +16,8 @@
 (function () {
   'use strict';
 
-  // ---- CONFIGURE THESE TWO VALUES ----
-  const CONFIG_URL = 'https://sldelacruz09808.github.io/toc-access-control.json';
+  // ---- CONFIGURE THIS VALUE ----
+  const CONFIG_URL = 'https://yourdomain.com/access-control.json';
   const GRACE_PERIOD_MS = 5 * 24 * 60 * 60 * 1000; // 5 days
   // -------------------------------
 
