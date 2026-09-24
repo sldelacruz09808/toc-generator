36137dd9cc513631c2f675e1db910919ae40e028
Bri808 <sldelacruz09@gmail.com>
2026-09-23 14:25:47 -1000

    Update license-check.js

--- license-check.js
+++ license-check.js
@@ -17,7 +17,7 @@
   'use strict';
 
   // ---- CONFIGURE THESE TWO VALUES ----
-  const CONFIG_URL = 'https://sldelacruz09808.github.io/app-distribution/access-control.json';
+  const CONFIG_URL = 'https://sldelacruz09808.github.io/app-distribution/toc-access-control.json';
   const GRACE_PERIOD_MS = 5 * 24 * 60 * 60 * 1000; // 5 days';
   // -------------------------------
 
