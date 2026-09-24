2dc258bb4e65da3ad54e31e66b2e1023bced357c
Bri808 <sldelacruz09@gmail.com>
2026-09-24 10:53:58 -1000

    Update license-check.js

--- license-check.js
+++ license-check.js
@@ -17,7 +17,7 @@
   'use strict';
 
   // ---- CONFIGURE THIS VALUE ----
-  const CONFIG_URL = 'https://github.io';
+  const CONFIG_URL = 'https://sldelacruz09808.github.io/toc-generator/';
   const GRACE_PERIOD_MS = 5 * 24 * 60 * 60 * 1000; // 5 days
   // -------------------------------
 
